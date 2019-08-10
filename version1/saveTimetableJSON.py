f78b0a62ee6a2216f2d1455a7a85fa6a34b6f7ac
Huakun Shen <shenhuakun@outlook.com>
2019-08-10 03:00:32 -0500

    modified SQL file.

--- version1/saveTimetableJSON.py
+++ version1/saveTimetableJSON.py
@@ -1,12 +1,14 @@
 import requests
 import json
 params = {
-    'code': 'CSC209'
+    'code': 'CSC'
 }
 r = requests.get('https://timetable.iit.artsci.utoronto.ca/api/20199/courses', params=params)
 r_dict = r.json()
-print(r_dict)
-print(r.url)
+print(len(r_dict))
+for key in r_dict.keys():
+    print(key)
+# print(r.url)
 # save json file locally
 with open('tmp.json', 'w') as f:
     json.dump(r_dict, f)
--- version1/timetableJSONtoDict.py
+++ version1/timetableJSONtoDict.py
@@ -4,7 +4,8 @@ import json
 params = {
     'code': 'CSC209'
 }
-r = requests.get('https://timetable.iit.artsci.utoronto.ca/api/20199/courses', params=params)
+r = requests.get(
+    'https://timetable.iit.artsci.utoronto.ca/api/20199/courses', params=params)
 r_dict = r.json()
 print(type(r_dict))
 
@@ -17,24 +18,14 @@ r_dict_keys = list(r_dict.keys())
 full_course_code = r_dict_keys[0]
 course = r_dict[full_course_code]
 
-# courseId = course['courseId']
-# code = course['code']
-# org = course['org']
-# orgName = course['orgName']
-# courseTitle = course['courseTitle']
-# courseDescription = course['courseDescription']
-# prerequisite = course['prerequisite']
-# corequisite = course['corequisite']
-# exclusion = course['exclusion']
-# recommendedPreparation = course['recommendedPreparation']
-# section = course['section']
-# session = course['session']
-# breadthCategories = course['breadthCategories']
-
 course_data_dict = {}
-course_keys = ['courseId', 'code', 'org', 'orgName', 'courseTitle', 'courseDescription', 'prerequisite', 'corequisite',
+course_keys = ['courseId', 'code', 'org', 'orgName', 'courseTitle', 'courseDescription',
+               'prerequisite', 'corequisite',
                'exclusion', 'recommendedPreparation', 'section', 'session', 'breadthCategories']
 for key in course_keys:
     course_data_dict[key] = course[key]
+
 print(course_data_dict)
+print(list(course_data_dict))
+print(list(course_data_dict.values()))
 # parse lecture/tut sections
